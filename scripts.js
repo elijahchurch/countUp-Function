0d702f7aba9920d516c778f6320255dd6c1ff42c
jason <elijahchurch@gmail.com>
2023-06-07 15:44:08 -0700

    update function Co-Authored by: Michael Carroll mcarroll138@gmail.com

--- scripts.js
+++ scripts.js
@@ -5,9 +5,12 @@ function countUp(countTo, countBy) {
     const badInputs = ["", NaN];
    if (badInputs.includes(parseInt(countTo)) || badInputs.includes(parseInt(countBy))) {
     return "Enter a number!";
+   } else if (Math.sign(countTo) === -1 || Math.sign(countBy) === -1) {
+    return "Enter a positive number!";
    }
     for (let i = countBy; i <= countTo; i +=countBy) {
         finalResult.push(i);
     }
     return finalResult;
+    
 };
